7d4316ea224e28ed9919c7fe809a50c7da012296
MMoina14 <miguelamoina@gmail.com>
2025-07-08 21:18:27 -0400

    burger

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+// Step 1: Variable Data
+
+const burgers = ['Hamburger', 'Cheeseburger']; // must use const
+let featuredDrink = 'Strawberry Milkshake';   // must use let
+
+// Step 2: Function and Block
+
+function addBurger() {
+  const newBurger = 'Flatburger';  // must use const
+  burgers.push(newBurger);
+}
+
+// Step 3: Variable Declaration and Scope
+
+if (true) {
+  const anotherNewBurger = 'Maple Bacon Burger';  // must use const with exact casing
+  burgers.push(anotherNewBurger);
+
+  function changeFeaturedDrink() {
+    featuredDrink = 'The JavaShake';
+  }
+}
